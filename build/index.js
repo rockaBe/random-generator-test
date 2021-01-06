5ebf0d4611ce074b3b59b6f3f1c222ac403fee76
Bernd Naehler <bnaehler@babbel.com>
2021-01-06 11:52:54 +0100

    feat: use imported module v0.0.2 and compile to es5

---
+++ build/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _rockabeRandonNumber = require('rockabe-randon-number');
+
+var _rockabeRandonNumber2 = _interopRequireDefault(_rockabeRandonNumber);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log((0, _rockabeRandonNumber2.default)(1, 10));
